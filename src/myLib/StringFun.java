d3367c1a6f5650f2ce33e9d8e0d0198c6956e9bd
Nevet one <patryk.opas.g@gmail.com>
2022-03-05 19:53:00 +0100

    Poprawki Palindrome i Camelize

--- src/myLib/StringFun.java
+++ src/myLib/StringFun.java
@@ -1,47 +1,6 @@
 package myLib;
 public class StringFun {
 
-    public static boolean IsPalindrome(String word){
-        String w2 = "";
-        for(int i = word.length()-1; i != -1; i--){
-            w2 += word.charAt(i);
-        }
-        if(w2.equals(word)){
-            return true;
-        }
-        return false;
-    }
-
-    public static String Anarchize(String text) {
-        char[] w2 = text.toCharArray();
-
-        for(int i = 0; i < w2.length; i++){
-            if(Character.isUpperCase(w2[i])){
-                w2[i] = Character.toLowerCase(w2[i]);
-            }else{
-                w2[i] = Character.toUpperCase(w2[i]);
-            }
-        }
-        return new String(w2);
-    }
-
-    public static String Camelize(String text) {
-        char[] w2 = text.toCharArray();
-        for(int i = 0; i < w2.length; i++){
-            if(i == 0 && Character.isUpperCase(w2[i])){
-                w2[i] = Character.toLowerCase(w2[i]);
-            }
-
-            if(w2[i] == ' ' && w2.length != i+1){
-                w2[i+1] = Character.toUpperCase(w2[i+1]);
-            }
-        }
-        String w2String = new String(w2);
-        w2String = w2String.replace(" ", ""); 
-        return w2String;
-    }
-
-
     public static String addChar(String str, char ch, int position) {
         return str.substring(0, position) + ch + str.substring(position);
     }
@@ -57,28 +16,79 @@ public class StringFun {
         return newarr;
     }
 
+    public static boolean IsPalindrome(String wordIn){
+        String wOut = "";
+        char[] wChar = wordIn.toCharArray();
+        for(int i = 0; i < wChar.length; i++){
+            if(Character.isUpperCase(wChar[i])){
+                wChar[i] = Character.toLowerCase(wChar[i]);
+            }
+        }
+        String word = (new String(wChar)).replace(" ", "");
+        for(int i = word.length()-1; i != -1; i--){
+            wOut += word.charAt(i);
+        }
+        if((wOut.replace(" ", "")).equals(word)){
+            return true;
+        }
+        return false;
+    }
+
+    public static String Anarchize(String text) {
+        char[] wTemp = text.toCharArray();
+
+        for(int i = 0; i < wTemp.length; i++){
+            if(Character.isUpperCase(wTemp[i])){
+                wTemp[i] = Character.toLowerCase(wTemp[i]);
+            }else{
+                wTemp[i] = Character.toUpperCase(wTemp[i]);
+            }
+        }
+        return new String(wTemp);
+    }
+
+    public static String Camelize(String text) {
+        char[] wTemp = text.toCharArray();
+        for(int i = 0; i < wTemp.length; i++){
+            if(i == 0 && Character.isUpperCase(wTemp[i])){
+                wTemp[i] = Character.toLowerCase(wTemp[i]);
+            }
+
+            if(wTemp[i] == ' ' && wTemp.length != i+1){
+                wTemp[i+1] = Character.toUpperCase(wTemp[i+1]);
+            }
+        }
+        String wTempString = new String(wTemp);
+        wTempString = wTempString.replace(" ", ""); 
+        return wTempString;
+    }
+
+
     public static String Decamelize(String text) {
-        char[] w2 = text.toCharArray();
+        char[] wTemp = text.toCharArray();
         int[] upperPosition = {};
         int addedSpaces = 0;
-        for(int i = 0; i < w2.length; i++){
+        for(int i = 0; i < wTemp.length; i++){
+            if(Character.isUpperCase(wTemp[0])){
+                continue;
+            }
 
-            if(Character.isUpperCase(w2[i])){
-                w2[i] = Character.toLowerCase(w2[i]);
+            if(Character.isUpperCase(wTemp[i])){
+                wTemp[i] = Character.toLowerCase(wTemp[i]);
                 upperPosition = addToArray(upperPosition.length, upperPosition, i+addedSpaces);
                 addedSpaces++;
             }
 
-            if(i == 0 && Character.isLowerCase(w2[i])){
-                w2[i] = Character.toUpperCase(w2[i]);
+            if(i == 0 && Character.isLowerCase(wTemp[i])){
+                wTemp[i] = Character.toUpperCase(wTemp[i]);
             }
         }
-        String w2String = new String(w2);
+        String wTempString = new String(wTemp);
         for(int i = 0; i < upperPosition.length; i++){
-            w2String = addChar(w2String, ' ', upperPosition[i]);
+            wTempString = addChar(wTempString, ' ', upperPosition[i]);
         }
 
-        return w2String;
+        return wTempString;
     }
 
 
